25af702743deb7b6b71e3dc09e7f220a6006f9e3
JPetta <jovipetraratulangi@gmail.com>
2019-04-09 11:47:29 +0700

    first commit

---
+++ DemoConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DemoConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Data type can't be converted");
+            }
+        }
+    }
+}
